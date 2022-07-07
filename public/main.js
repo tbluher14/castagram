e902a2686d993b5b0b3d4be8c25338d6fd109c3f
Tom Bluher <tombluher@Toms-MacBook-Air.local>
2022-07-07 15:34:39 -0600

    phase II solved

--- public/main.js
+++ public/main.js
@@ -24,6 +24,7 @@ const fetchImage = async () => {
         // console.log(kittenData);
         const kittenImg = document.querySelector("img");
         kittenImg.src = kittenData[0].url;
+
     } catch (e) {
         console.log("Failed to fetch image", e);
     }
@@ -37,6 +38,8 @@ button.style.height = "40px"
 button.innerText = 'New Cat'
 button.addEventListener("click", fetchImage)
 
+
+
 // Up Vote Button
 let clicks = 0;
 const upCounter = document.createElement("button")
@@ -46,15 +49,19 @@ upCounter.style.height = "40px"
 upCounter.innerText = 'Up Vote'
 upCounter.style.display = "flex"
 window.addEventListener("DOMContentLoaded", event => {
-    const count = document.getElementById("clicked-count");
     upCounter.addEventListener("click", event => {
       clicks += 1;
       let totalvote = document.querySelector('.totalvote')
       totalvote.innerHTML = clicks;
-      totalvote.innerHTML = `<span>${clicks}</span>`
+    //   totalvote.innerHTML = `<span>${clicks}</span>`
     });
   });
 
+  // reset votes
+button.addEventListener("click", event => {
+
+    totalvote.innerHTML = 0
+})
 
 // Down Vote Button
 const downCounter = document.createElement("button")
@@ -65,11 +72,10 @@ downCounter.style.display = "flex"
 downCounter.innerText = 'Down Vote'
 window.addEventListener("DOMContentLoaded", event => {
   downCounter.addEventListener("click", event => {
-      const count = document.getElementById("clicked-count");
       clicks += 1;
       let totalvote = document.querySelector('.totalvote')
       totalvote.innerHTML = clicks;
-      totalvote.innerHTML = `<span>${clicks}</span>`
+    //   totalvote.innerHTML = `<span>${clicks}</span>`
     });
   });
 
@@ -79,35 +85,39 @@ span.setAttribute('id', 'clicked-count')
 
 const votecount = document.createElement("button")
 votecount.className = 'totalvote'
-votecount.innerHTML = `<span>${clicks}</span>`
-// votecount.appendChild(span)
-// votecount.setAttribute('span', 'clicked-count')
+// votecount.innerHTML = `<span>${clicks}</span>`
 document.body.append(votecount)
 let totalvote = document.querySelector('.totalvote')
 totalvote.innerText = clicks
 votecount.style.width = "75px"
 votecount.style.height = "40px"
 votecount.style.display = "flex"
-// votecount.innerText = 'Vote Count'
 
+
+// Comments Section
+
+const commentInput = document.createElement("input")
+document.body.append(commentInput)
+commentInput.style.width = "250px"
+commentInput.style.height = "200px"
+
+// Button to Submit a Comment
+const commentSubmit = document.createElement("button")
+document.body.append(commentSubmit)
+commentSubmit.style.width = "75px"
+commentSubmit.style.height = "50px"
+commentSubmit.innerText = "Submit Comment"
+
+// Comment's Section
+const commentSection = document.createElement("ul")
+document.body.append(commentSection)
+
+// Event to Post comment
 window.addEventListener("DOMContentLoaded", event => {
-    const originalText = document.getElementById("add");
-    originalText.addEventListener("input", event => {
-      let text = originalText.value;
-    })
-  });
-
-window.addEventListener("DOMContentLoaded", event => {
-    console.log('domcontent loaded')
-    const pastComments = document.getElementById("unorderedList")
-    const addCommentButton = document.getElementById("add")
-    const unorderedList = document.createElement("ul")
-
-    addItem.addEventListener("click", e => {
+    commentSubmit.addEventListener("click", e => {
         e.preventDefault()
-        const comment = document.getElementById("name")
         const li = document.createElement("li")
-        li.innerText = name.value
-        pastComments.appendChild(li)
+        li.innerText = commentInput.value
+        commentSection.appendChild(li)
     })
 })
